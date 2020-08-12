c35af0c2beed4cd1fc624dab9d4d512813fdf354
MayreeOsuyak <osuyakmayree@gmail.com>
2020-08-12 16:34:02 +0900

    second commit

---
+++ JS/quiz.js
@@ -0,0 +1,67 @@
+function play(){
+    let put
+    put = document.getElementById("c").checked;
+
+    if (put == true){
+        document.getElementById("h1").innerHTML="&#10004Correct!!"
+        document.getElementById("h1").style.color="green"
+    }
+    else{
+        document.getElementById("h1").innerHTML="&#10060Incorrect!!"
+        document.getElementById("h1").style.color="red" 
+    }
+}
+function play1(){
+    let put
+    put = document.getElementById("d3").checked;
+
+    if (put == true){
+        document.getElementById("h2").innerHTML="&#10004Correct!!"
+        document.getElementById("h2").style.color="green"
+    }
+    else{
+        document.getElementById("h2").innerHTML="&#10060Incorrect!!"
+        document.getElementById("h2").style.color="red" 
+    }
+}
+function play2(){
+    let put
+    put = document.getElementById("e1").checked;
+
+    if (put == true){
+        document.getElementById("h3").innerHTML="&#10004Correct!!"
+        document.getElementById("h3").style.color="green"
+    }
+    else{
+        document.getElementById("h3").innerHTML="&#10060Incorrect!!"
+        document.getElementById("h3").style.color="red" 
+    }
+}
+function play3(){
+    let put
+    put = document.getElementById("f3").checked;
+
+    if (put == true){
+        document.getElementById("h4").innerHTML="&#10004Correct!!"
+        document.getElementById("h4").style.color="green"
+    }
+    else{
+        document.getElementById("h4").innerHTML="&#10060Incorrect!!"
+        document.getElementById("h4").style.color="red" 
+    }
+}
+function play4(){
+    let put
+    put = document.getElementById("g").checked;
+
+    if (put == true){
+        document.getElementById("h5").innerHTML="&#10004Correct!!"
+        document.getElementById("h5").style.color="green"
+    }
+    else{
+        document.getElementById("h5").innerHTML="&#10060Incorrect!!"
+        document.getElementById("h5").style.color="red" 
+    }
+}
+
+
